766e78816b65362f9deff8a9e377224435e8d722
IRISHREX <Sohelislam993@gmail.com>
2026-08-14 13:11:35 +0530

    Fix dashboard widget models autoloading and RBAC model resolution for admin login

--- smart_hospital_src/application/config/autoload.php
+++ smart_hospital_src/application/config/autoload.php
@@ -127,4 +127,4 @@ $autoload['language'] = array();
   |
   |	$autoload['model'] = array('first_model' => 'first');
  */
-$autoload['model'] = array('setting_model', 'frontcms_setting_model', 'staff_model', 'language_model', 'userlog_model', 'role_model');
+$autoload['model'] = array('setting_model', 'frontcms_setting_model', 'staff_model', 'staffroles_model', 'rolepermission_model', 'language_model', 'userlog_model', 'role_model', 'notification_model', 'notificationsetting_model', 'customfield_model', 'systemnotification_model', 'expense_model', 'income_model', 'patient_model', 'appointment_model');
--- smart_hospital_src/application/libraries/Rbac.php
+++ smart_hospital_src/application/libraries/Rbac.php
@@ -32,7 +32,9 @@ class Rbac
 
     public function getPermission($role_id)
     {
-
+        if (!isset($this->CI->rolepermission_model)) {
+            $this->CI->load->model('rolepermission_model');
+        }
         $role_perm = $this->CI->rolepermission_model->getPermissionByRole($role_id);
 
         foreach ($role_perm as $key => $value) {
